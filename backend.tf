119ce7a84be34378ea8539c140c81ad84e79878b
Samson <samsonispower@gmail.com>
2026-07-29 13:28:33 +0100

    Update key

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "sa-lab-terraform-state-151d5ba5"
-    key            = "infrastructure/terraform.tfstate"
+    key            = "sa-infrastructure/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-state-locks"
     encrypt        = true
