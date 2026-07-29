b7ace1602f641c8db3bb99b4e8b9fcb595c7905b
Samson <samsonispower@gmail.com>
2026-07-29 14:07:38 +0100

    Delete bootstrap and update backend block

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "sa-lab-terraform-state-a15f0c2e"
-    key            = "infrastructure/terraform.tfstate"
+     key            = "aws-sa-labs/infrastructure/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-state-locks"
     encrypt        = true
