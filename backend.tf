294ac664ab27b96af22dd3c53fce4f3ebb0a96bd
Samson <samsonispower@gmail.com>
2026-07-29 14:08:57 +0100

    Format backend terraform

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "sa-lab-terraform-state-a15f0c2e"
-     key            = "aws-sa-labs/infrastructure/terraform.tfstate"
+    key            = "aws-sa-labs/infrastructure/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-state-locks"
     encrypt        = true
