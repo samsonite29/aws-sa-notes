dc14f08fa07cd2efa2a28046f5313d0c2033e176
Samson <samsonispower@gmail.com>
2026-07-30 20:43:27 +0100

    Forrmat ec2 resource file

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
-module "ec2_instance"{
-    source = "./modules/server"
-    instance_type = "t2.micro"
-    user_data = file("user_data.sh")
+module "ec2_instance" {
+  source        = "./modules/server"
+  instance_type = "t2.micro"
+  user_data     = file("user_data.sh")
 }
