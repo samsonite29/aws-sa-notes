72b43dccef4195cdac3ce1bca685982b9a5bcb43
Samson <samsonispower@gmail.com>
2026-07-30 21:01:27 +0100

    Update data block for fetching AMI images

--- modules/server/main.tf
+++ modules/server/main.tf
@@ -1,10 +1,12 @@
 data "aws_ami" "image" {
   most_recent = true
   owners      = ["amazon"]
+
   filter {
     name   = "architecture"
-    values = ["arm64"]
+    values = ["x86_64"]
   }
+
   filter {
     name   = "name"
     values = ["al2023-ami-2023*"]
