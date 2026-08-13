064ffdbc751501631859a0e8a95146a6cc7655df
Samson <samsonispower@gmail.com>
2026-08-13 21:29:21 +0100

    Ignore ec2.tf files

--- ec2.tf
+++ ec2.tf
@@ -1,26 +1,26 @@
-module "instance_network" {
-  source = "./modules/network"
-}
+# module "instance_network" {
+#   source = "./modules/network"
+# }
 
 
 
-module "ec2_instance" {
-  source             = "./modules/server"
-  instance_type      = "t3.micro"
-  user_data          = file("user_data.sh")
-  subnet_id          = module.instance_network.subnet_id
-  security_group_ids = [module.instance_network.security_group_id]
-  zone               = module.instance_network.zones[0]
-  ebs_size           = 2
-}
+# module "ec2_instance" {
+#   source             = "./modules/server"
+#   instance_type      = "t3.micro"
+#   user_data          = file("user_data.sh")
+#   subnet_id          = module.instance_network.subnet_id
+#   security_group_ids = [module.instance_network.security_group_id]
+#   zone               = module.instance_network.zones[0]
+#   ebs_size           = 2
+# }
 
 
-module "ec2_instance_two" {
-  source             = "./modules/server"
-  instance_type      = "t3.micro"
-  user_data          = file("user_data.sh")
-  subnet_id          = module.instance_network.subnet_id
-  security_group_ids = [module.instance_network.security_group_id]
-  zone               = module.instance_network.zones[0]
-  ebs_size           = 2
-}
+# module "ec2_instance_two" {
+#   source             = "./modules/server"
+#   instance_type      = "t3.micro"
+#   user_data          = file("user_data.sh")
+#   subnet_id          = module.instance_network.subnet_id
+#   security_group_ids = [module.instance_network.security_group_id]
+#   zone               = module.instance_network.zones[0]
+#   ebs_size           = 2
+# }
